a27b24b4bcf5c59b2342c94c3f0fb7ca411ffd95
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-06-14 12:48:24 -0600

    Update React packages

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
+import './globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
