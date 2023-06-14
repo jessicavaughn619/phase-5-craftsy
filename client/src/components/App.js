6acff93e4b0a4b1410ec1cb265a971f8e82dac40
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-06-14 12:31:01 -0600

    Add index.js to client side

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
-
-function App() {
-  // Code goes here!
+export default function App() {
+  return (
+    <h1>Hello World!</h1>
+  )
 }
 
-export default App;
