0343af10b6f70c8e6ad3a21e28727e336abaefb7
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-07-05 12:49:05 -0600

    Update app.py to remove comments

--- server/app.py
+++ server/app.py
@@ -14,7 +14,6 @@ from oauthlib.oauth2 import WebApplicationClient
 import requests
 from flask_cors import CORS
 
-# Local imports
 from config import app, db, api
 from models import Product, User, Review, Order
 
@@ -56,12 +55,9 @@ def get_google_provider_cfg():
 
 @app.route("/login")
 def login():
-    # Find out what URL to hit for Google login
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
-    # Use library to construct the request for login and provide
-    # scopes that let you retrieve user's profile from Google
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
         redirect_uri=request.base_url + "/callback",
@@ -71,15 +67,11 @@ def login():
 
 @app.route("/login/callback")
 def callback():
-    # Get authorization code Google sent back to you
     code = request.args.get("code")
 
-    # Find out what URL to hit to get tokens that allow you to ask for
-    # things on behalf of a user
     google_provider_cfg = get_google_provider_cfg()
     token_endpoint = google_provider_cfg["token_endpoint"]
 
-    # Prepare and send request to get tokens! Yay tokens!
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response=request.url,
@@ -93,19 +85,12 @@ def callback():
         auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
     )
 
-    # Parse the tokens!
     client.parse_request_body_response(json.dumps(token_response.json()))
 
-    # Now that we have tokens (yay) let's find and hit URL
-    # from Google that gives you user's profile information,
-    # including their Google Profile Image and Email
     userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
     uri, headers, body = client.add_token(userinfo_endpoint)
     userinfo_response = requests.get(uri, headers=headers, data=body)
 
-    # We want to make sure their email is verified.
-    # The user authenticated with Google, authorized our
-    # app, and now we've verified their email through Google!
     if userinfo_response.json().get("email_verified"):
         unique_id = userinfo_response.json()["sub"]
         users_email = userinfo_response.json()["email"]
@@ -114,13 +99,9 @@ def callback():
     else:
         return "User email not available or not verified by Google.", 400
 
-    # Create a user in our db with the information provided
-    # by Google
     user = User(
         id=unique_id, first_name=users_name, email=users_email, profile_pic=picture
     )
-
-    # Doesn't exist? Add to database
     if not User.get(unique_id):
         user = User(
             id=unique_id, 
@@ -130,10 +111,7 @@ def callback():
         )
         db.session.add(user)
         db.session.commit()
-    # Begin user session by logging the user in
     login_user(user)
-
-    # Send user back to homepage
     return redirect('http://localhost:4000/')
 
 class Logout(Resource):
