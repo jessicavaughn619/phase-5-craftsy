c2e40a5b688d60bf8260836054e8bc489c74be91
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-08-01 20:33:48 -0600

    Revert

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,22 +1,6 @@
 import ProductCard from "./ProductCard"
-import { useEffect } from "react"
 
 export default function Products({ products, productsInCart, onSetProductsInCart, onSetMessage }) {
-
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const check_session_response = await fetch("/api/");
-            if (check_session_response.ok) {
-              const user = await check_session_response.json();
-              setUser(user);
-            }
-          } catch (error) {
-            console.log(error)
-          }
-        }
-        fetchData()
-      }, []);
 
     const allProducts = products.map((product) => (
         <ProductCard 
