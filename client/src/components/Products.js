b564286f04410eaf4a3330c33689bba5925d3e47
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-08-03 16:53:17 -0600

    Update products media query

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -12,7 +12,7 @@ export default function Products({ products, productsInCart, onSetProductsInCart
         />
     ))
     return (
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 gap-4 xl:grid-cols-2 2xl:grid-cols-3">
             {allProducts}
         </div>
     )
