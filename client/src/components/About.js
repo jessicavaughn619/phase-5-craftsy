9a2644e4ffa754e9e5e05b5ef94c15003670dd4d
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-08-04 10:59:43 -0600

    Update about photo size to increase with query

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom'
 
 export default function About() {
     return (
-        <div className="m-5 mx-10 max-width-full flex flex-col justify-between">
+        <div className="m-5 mx-10 max-width-full flex flex-col justify-between gap-4">
             <div className="flex flex-col justify-between shadow-lg sm:flex-row">
-                <img className="object-cover h-96 w-48 rounded-md self-center" src={sandy_doyle} alt="Sandy & Doyle avatar"></img>
+                <img className="object-cover h-96 w-48 rounded-md self-center sm:w-2/4" src={sandy_doyle} alt="Sandy & Doyle avatar"></img>
             <div className="flex flex-col justify-evenly p-5">
                 <p>Craftsy is a small-business owned and operated by Sandy and Doyle.</p>
                 <p>Doyle and Sandy are a part of the entire process for their ceramics - from pouring slip into molds to firing the product for the final time, they take pride in creating custom, unique products.</p>
@@ -20,10 +20,10 @@ export default function About() {
                     <a className="hover:text-amber-600" href="https://visitlajunta.net/" target="_blank" rel="noreferrer"> La Junta, Colorado!</a> Consider passing through 'the junction' when planning your next road trip!</p>
                     <p>Thank you for visiting Craftsy and for supporting small-businesses!</p>
                 </div>
-                <img className="object-cover w-48 h-96 rounded-md self-center" src={backyard} alt="La Junta, CO landscape"></img>
+                <img className="object-cover w-48 h-96 rounded-md self-center sm:w-2/4" src={backyard} alt="La Junta, CO landscape"></img>
             </div>
             <div className="flex flex-col justify-between shadow-lg sm:flex-row">
-                <img className="object-cover w-48 h-96 rounded-md self-center" src={jessica} alt="Jessica avatar"></img>
+                <img className="object-cover w-48 h-96 rounded-md self-center sm:w-2/4" src={jessica} alt="Jessica avatar"></img>
                 <div className="flex flex-col justify-evenly p-5">
                     <p className="pb-2">Craftsy's website is designed, built, and maintained by Sandy and Doyle's daughter, Jessica.</p>
                     <Button children={
