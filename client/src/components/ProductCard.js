09c0dba8bcefb7f3bbba5689ae0531818d6a808c
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-08-16 13:24:42 -0600

    Update tooltip to default style

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -77,10 +77,11 @@ export default function ProductCard({ product, productsInCart, onSetProductsInCa
             <div className="relative">
                 <img className="object-contain" src={image} alt={item}/>
                 <div className={`absolute inset-0 h-full w-full bg-white transition-all ease-in-out duration-300 ${isHover ? "bg-opacity-60" : "bg-opacity-0"}`}>
-                    <div className={`absolute top-1/2 left-1/2 transition-all duration-300 z-1 -translate-x-1/2 ${isHover ? "-translate-y-1/2 opacity-100" : "-translate-y-20 opacity-0"}`}>
-                        <FiMoreHorizontal onClick={handleReviewClick} data-tooltip-target="tooltip-light" data-tooltip-trigger="hover" data-tooltip-placement="top" className="cursor-pointer text-4xl bg-white rounded-full transition-all duration-300 hover:text-white hover:bg-amber-600" />
-                        <div id="tooltip-light" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-amber-600 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 hover:opacity-100 hover:visible tooltip">
+                    <div className={`absolute top-1/2 left-1/2 transition-all duration-300 z-1 -translate-x-1/2 ${isHover ? "-translate-y-1/2 opacity-100" : "translate-y-20 opacity-0"}`}>
+                        <FiMoreHorizontal onClick={handleReviewClick} data-tooltip-target="tooltip-default" data-tooltip-trigger="hover" data-tooltip-placement="top" className="cursor-pointer text-4xl bg-white rounded-full transition-all duration-300 hover:text-white hover:bg-amber-600" />
+                        <div id="tooltip-default" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
                         More Info
+                        <div class="tooltip-arrow" data-popper-arrow></div>
                         </div>
                     </div>
                  </div>
