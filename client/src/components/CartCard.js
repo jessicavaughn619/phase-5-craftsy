efe333d1dbcc473f740950b8870b497bccbd7a40
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-08-16 15:47:41 -0600

    Style cart card

--- client/src/components/CartCard.js
+++ client/src/components/CartCard.js
@@ -48,10 +48,12 @@ export default function CardCard({ product, onDeleteItem, onUpdateQuantityInCart
     return (
         <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="grid grid-template-col-auto-1fr rounded shadow-lg gap-2">
             <img className="object-contain h-48 p-2" src={image} alt={item}></img>
-            <div className="flex flex-col py-4 w-full">
-                <div className="font-bold text-md">{item}</div>
-                <p className="text-gray-700 text-base text-sm mb-2">{description}</p>
-                <div className="flex justify-between w-full overflow-hidden">
+            <div className="flex flex-col justify-between p-2">
+                <div>
+                    <div className="font-bold text-md">{item}</div>
+                    <p className="text-gray-700 text-base text-sm mb-2">{description}</p>
+                </div>
+                <div className="flex justify-between items-end overflow-hidden">
                     <div className="text-amber-600 text-bold absolute">${price.toFixed(2)}</div>
                     <div className={`flex space-x-2 items-center cursor-pointer relative bg-white z-1 transition ease-in-out duration-300 ${isHover ? "translate-x-0" : "-translate-x-full"}`}>
                         <BsCartX onClick={handleClick} className="inline-block text-lg text-amber-600"/>
@@ -59,10 +61,10 @@ export default function CardCard({ product, onDeleteItem, onUpdateQuantityInCart
                     </div>
                     <div className="flex flex-col">
                         <div>In Stock: {quantity}</div>
-                        <div className="space-x-2 py-1 px-2 border rounded-lg">
-                            <button onClick={handleDecrementClick} className={quantityInCart > 1 ? "cursor-pointer visible" : "invisible"}>-</button>
+                        <div className="flex space-x-2 py-1 justify-center border rounded-lg">
+                            <button onClick={handleDecrementClick} className={quantityInCart > 1 ? "cursor-pointer opacity-100" : "opacity-25 cursor-not-allowed"}>-</button>
                             <span>{quantityInCart}</span>
-                            <button onClick={handleIncrementClick} className={quantityInCart < quantity ? "cursor-pointer visible" : "invisible"}>+</button>
+                            <button onClick={handleIncrementClick} className={quantityInCart < quantity ? "cursor-pointer opacity-100" : "opacity-25 cursor-not-allowed"}>+</button>
                         </div>
                     </div>
                 </div>
