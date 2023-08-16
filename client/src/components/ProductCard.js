4130aacd76d904e50fc70fab38e35afa84cc935a
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-08-16 12:25:53 -0600

    Add flowbite script to index.html, update more info logo styling

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -78,10 +78,11 @@ export default function ProductCard({ product, productsInCart, onSetProductsInCa
                 <img className="object-contain" src={image} alt={item}/>
                 <div className={`absolute inset-0 h-full w-full bg-white transition-all ease-in-out duration-300 ${isHover ? "bg-opacity-60" : "bg-opacity-0"}`}>
                     {isHover && (
-                        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-1">
-                            <CiCircleMore onClick={handleReviewClick} data-tooltip-target="tooltip-light" data-tooltip-trigger="hover" data-tooltip-placement="top" className="cursor-pointer text-5xl hover:text-amber-600" />
-                            <div id="tooltip-light" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-amber-600 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 tooltip">
+                        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-1 transition-opacity duration-300">
+                            <CiCircleMore onClick={handleReviewClick} data-tooltip-target="tooltip-light" data-tooltip-trigger="hover" data-tooltip-placement="top" className="cursor-pointer text-5xl bg-white rounded-full transition-colors duration-300 hover:text-white hover:bg-amber-600" />
+                            <div id="tooltip-light" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-amber-600 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 tooltip">
                             More Info
+                            <div class="tooltip-arrow" data-popper-arrow></div>
                             </div>
                         </div>
                     )}
