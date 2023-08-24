7868307535729a8bb6f8ff66a5a3f5d888e9906f
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-08-23 19:10:52 -0600

    Add z-20 to Menu

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -2,7 +2,7 @@ import NavBar from "./NavBar"
 
 export default function Menu({ isMenuOpen, onSetIsMenuOpen, onSetUser, onSetMessage, productsInCart, onSetProductsInCart }) {
     return (
-        <div className={`top-0 z-1 absolute transition-transform duration-300 ease bg-white h-screen w-full text-xl ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
+        <div className={`top-0 z-20 absolute transition-transform duration-300 ease bg-white h-screen w-full text-xl ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
         <NavBar 
             isMenu={true}
             onSetIsMenuOpen={onSetIsMenuOpen}
