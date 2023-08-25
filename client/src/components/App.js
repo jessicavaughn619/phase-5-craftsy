26be0b59fe7ffbde8b4ed24aaddf56c33de9e253
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-08-25 11:02:45 -0600

    Review review console log, debugged add review issue

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -150,7 +150,6 @@ export default function App() {
   }
 
   function handleAddReview(review) {
-    console.log(review)
     const productToUpdate = products.find(product => (product.id===review.product_id))
     const updatedProducts = products.map(product => {
       if (product.id===productToUpdate.id) {
